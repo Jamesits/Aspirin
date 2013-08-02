4bc06d6b06767824b22a2aa3f01f73de940a316e
zhj <911354359@qq.com>
2013-08-02 21:41:11 +0800

    Color selector now fully working.

--- www/js/about.js
+++ www/js/about.js
@@ -6,16 +6,21 @@ $(function(){
                  //$(".color").parents("li").buttonMarkup({ icon: "false" });
                  //$(".color").closest("ul").listview({ icon: false });
                  //$(".color").closest("ul").listview("refresh");
+                 //$(".color").parents("li").jqmData('icon','false');
+                 $(".color").parents("li").buttonMarkup({ icon: false });
                  window.localStorage.setItem("Colorscheme",$(e.target).attr("data-value"));
                  //alert($(e.target).attr("data-value"));
                  $(e.target).parents("li").buttonMarkup({ icon: "check" });
                  });
   
+  $(document).on("mobileinit", function(){
+                 $.mobile.defaultPageTransition = 'slide';
+                 $.mobile.page.prototype.options.domCache = true;
+                 //$.mobile.listview.prototype.options.icon = false;
+                 //$(".color").eq(window.localStorage.getItem("Colorscheme")).parents("li").buttonMarkup({ icon: "check" });
+                 });
+  $( document ).on( "pageinit", "#color", function( event ) {
+                   $(".color").parents("li").buttonMarkup({ icon: false });
+                   $(".color").eq(window.localStorage.getItem("Colorscheme")).parents("li").buttonMarkup({ icon: "check" });
+                   });
   });
-$(document).on("mobileinit", function(){
-               $.mobile.defaultPageTransition = 'slide';
-               $.mobile.page.prototype.options.domCache = true;
-               //$.mobile.listview.prototype.options.icon = false;
-               //$(".color").eq(window.localStorage.getItem("Colorscheme")).parents("li").buttonMarkup({ icon: "check" });
-               $(".color").eq(window.localStorage.getItem("Colorscheme")).parents("li").attr("data-icon","check");
-               });
